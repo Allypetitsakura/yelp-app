2db9a467e9f322a46914937539df3c19aae6c9f2
Alessia Garbi <lalebi@live.com>
2021-11-10 15:32:11 +0100

    seeds done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database ...'
+Restaurant.destroy_all
+
+5.times do
+  restaurant = Restaurant.create(
+    name: Faker::JapaneseMedia::Naruto.village,
+    address: Faker::Movies::HarryPotter.location,
+    phone_number: Faker::Number.number(digits: 10),
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  puts "Created restaurant ##{restaurant.id}"
+end
